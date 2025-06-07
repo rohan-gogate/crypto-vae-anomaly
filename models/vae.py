b37d42f40e4d904e6e4c76e29a0c7f0effc47049
rohan-gogate <rohgogate@gmail.com>
2025-06-07 16:31:37 -0400

    Finished vae class, ready to train

--- models/vae.py
+++ models/vae.py
@@ -1,5 +1,43 @@
 import torch 
 import torch.nn as nn
+import torch.nn.functional as F
 
 class VAE(nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim):)
+    def __init__(self, input_dim=60, latent_dim=5, hidden_dim=32):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+
+        self.mu_layer = nn.Linear(hidden_dim, latent_dim)
+        self.logvar_layer = nn.Linear(hidden_dim, latent_dim)
+    
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim)
+        )
+        
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+    
+    def forward (self, x):
+        encoded = self.encoder(x)
+        mu = self.mu_layer(encoded)
+        logvar = self.logvar_layer(encoded)
+        z = self.reparameterize(mu, logvar)
+        reconstructed = self.decoder(z)
+        return reconstructed, mu, logvar
+    
+    @staticmethod
+    def loss_function(reconstructed, x, mu, logvar, beta=1.0):
+        mse = F.mse_loss(reconstructed, x, reduction="mean")
+        kl_div = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+        return mse + beta*kl_div, mse, kl_div
